5eb7362b4d99349fc12efe9cd858c663a1dbaf3a
cymruhangu <pragquix@gmail.com>
2018-08-29 20:41:16 -0400

    fixed DATABASE_URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:D0nt4g3t!@ds133642.mlab.com:33642/blog-app';
-
+exports.DATABASE_URL = 
+  process.env.DATABASE_URL || "mongodb://localhost:27017/blog-app";
 exports.TEST_DATABASE_URL =
-   process.env.TEST_DATABASE_URL || 'mongodb://admin:D0nt4g3t!@ds133642.mlab.com:33642/test-blog-app';
-exports.PORT = process.env.PORT || 8080;
+   process.env.TEST_DATABASE_URL || "mongodb://localhost:27017/test-blog-app";
+   exports.PORT = process.env.PORT || 8080;
